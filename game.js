b674d6b1f0b9b40a1255045c2b12d25813d918c0
khaingmyozaw <kmyozaw.dev@gmail.com>
2025-09-04 23:44:54 +0630

    move human paddle

--- game.js
+++ game.js
@@ -5,8 +5,8 @@ const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 
 // Paddles' settings
-const PADDLE_WIDTH = 12;
-const PADDLE_HEIGHT = 90;
+const PADDLE_WIDTH = 94;
+const PADDLE_HEIGHT = 12;
 const PADDLE_MARGIN = 16;
 const PADDLE_SPEED = 6;
 
@@ -14,14 +14,33 @@ const PADDLE_SPEED = 6;
 const BALL_SIZE = 16;
 const BALL_SPEED = 6;
 
-const humanPaddleY = HEIGHT / 2 - PADDLE_HEIGHT / 2;
-const botPaddleY = HEIGHT / 2 - PADDLE_HEIGHT / 2;
+let humanPaddleX = WIDTH / 2 - PADDLE_WIDTH / 2;
+let botPaddleX = WIDTH / 2 - PADDLE_WIDTH / 2;
 
-const ballX = WIDTH / 2 - BALL_SIZE / 2;
-const ballY = HEIGHT / 2 - BALL_SIZE / 2;
+let ballX = WIDTH / 2 - BALL_SIZE / 2;
+let ballY = HEIGHT / 2 - BALL_SIZE / 2;
+let ballVX = BALL_SPEED * (Math.random() * 2 - 1);
+let ballVY = BALL_SPEED * (Math.random() > 0.5 ? 1 : -1);
+
+function clamp(val, min, max) {
+    return Math.max(min, Math.min(max, val));
+}
+
+// Move ball
+function moveBall() {
+    ballX += ballVX;
+    ballY += ballVY;
+}
+
+// Move human paddle
+canvas.addEventListener('mousemove', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+
+    humanPaddleX = clamp(mouseX - PADDLE_WIDTH / 2, 0, WIDTH - PADDLE_WIDTH);
+});
 
 // Draw game objects
-draw();
 function draw() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -29,15 +48,21 @@ function draw() {
     ctx.setLineDash([16, 18, 1]);
     ctx.strokeStyle = "#888";
     ctx.beginPath();
-    ctx.moveTo(WIDTH / 2, 0);
-    ctx.lineTo(WIDTH / 2, HEIGHT);
+    ctx.moveTo(0, HEIGHT / 2);
+    ctx.lineTo(WIDTH, HEIGHT / 2);
     ctx.stroke();
 
     // Draw Paddles
     ctx.fillStyle = "#fff";
-    ctx.fillRect(PADDLE_MARGIN, humanPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.fillRect(humanPaddleX, HEIGHT - PADDLE_MARGIN - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
     // Paddle Bot
-    ctx.fillRect(WIDTH - PADDLE_MARGIN - PADDLE_WIDTH, botPaddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+    ctx.fillRect(botPaddleX, PADDLE_MARGIN, PADDLE_WIDTH, PADDLE_HEIGHT);
 
     ctx.fillRect(ballX, ballY, BALL_SIZE, BALL_SIZE);
 }
+
+function gameLoop() {
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
